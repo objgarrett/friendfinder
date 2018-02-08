50c217062edc0c6ee18a057fc317a08f466cc114
Olivia Garrett <oliviajonesgarrett@gmail.com>
2018-02-08 16:37:58 -0500

    updates to js files

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendsArray = [
+    {
+        name: "Olivia",
+        photo: "www.google.com",
+        scores: [ 
+                    1, 
+                    2,
+                    3,
+                    4,
+                    5,
+                    1,
+                    2,
+                    3,
+                    4,
+                    5
+
+        ]
+    }
+];
+  
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,12 @@
+var friendsArray = require("../data/friends");
+
+module.exports = function (app) {
+    app.get("/api/friends", function(req, res) {
+        res.json(friendsArray);
+    });
+
+    app.post("/api/friends", function(req, res) {
+        friendsArray.push(req.body);
+        res.json(true);
+    });
+}
